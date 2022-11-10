f14dcc098e23ee3b944af63bea01c8f7f2d2d0b0
claes-e <erikssoncla@gmail.com>
2022-11-10 06:46:41 +0100

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+alert("You cant divide us")
+
+console.log("Foo");
+console.log("bar");
+let foo = 0;
+let bar = 0;
+let result = foo / bar;
+console.log(foo/bar)
+
+//borde väl vara typ alert.if men får det inte att funka
